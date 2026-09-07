3c3e98e87a24589f5cd719c135ce4e93ebece4f6
jmnllona <jeraldmarknievallona@outlook.com>
2026-09-07 21:27:55 +0800

    ui update

--- backend/src/routes/messageRoutes.ts
+++ backend/src/routes/messageRoutes.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import pool from "../db.js"
 import { createMessage, getPublicMessages, getAllMessages } from "../controllers/messageController.js"
 
 const router = Router();
@@ -6,4 +7,26 @@ const router = Router();
 router.post("/messages", createMessage);
 router.get("/public-messages", getPublicMessages);
 router.get("/all-messages", getAllMessages)
+
+
+router.post("/visit", async (_req, res) => {
+    try {
+        await pool.query(
+            "INSERT INTO visitors (viewed) VALUES ('1')"
+        );
+
+        res.status(201).json({
+            message: "Visit recorded"
+        });
+
+    } catch (error) {
+        console.error(error);
+
+        res.status(500).json({
+            message: "Failed to record visit"
+        });
+    }
+});
+
+
 export default router;
